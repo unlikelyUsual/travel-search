161b94901ff86e438c66d8ba786a6f1aa785c095
unlikelyUsual <heywhat.sup308@gmail.com>
2023-08-21 01:01:32 +0530

    Build scripts

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import Database from "./config/database";
 import tripRoute from "./routes/trip";
 import cors from "cors";
+import path from "path";
 
 dotenv.config({ debug: true });
 
@@ -15,6 +16,14 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", tripRoute);
+
+if (process.env.NODE_ENV === "prod") {
+  app.use(express.static("../client/build"));
+
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
