5bc6c58c74e3f28c223bac47af9ae2219c75e09f
unlikelyUsual <heywhat.sup308@gmail.com>
2023-08-20 14:00:57 +0530

    Client Form and utils

---
+++ client/src/util/alerts.ts
@@ -0,0 +1,14 @@
+import toast, { Toaster } from "react-hot-toast";
+
+class Alert {
+  static success(message: string) {
+    toast.success(message);
+  }
+
+  static fail(message: string, error?: any) {
+    console.log(`Error => `, error);
+    toast.error(message);
+  }
+}
+
+export default Alert;
---
+++ client/src/util/axios.ts
@@ -0,0 +1,19 @@
+import Axios from "axios";
+
+const axios = Axios.create({
+  baseURL: "https://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axios.interceptors.request.use((config) => {
+  console.log(config.data);
+  return config;
+});
+axios.interceptors.response.use((response) => {
+  console.log(response);
+  return response;
+});
+
+export default axios;
